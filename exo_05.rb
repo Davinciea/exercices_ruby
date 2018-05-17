f70c2ecf96a9a4e934ce93abb19c1ea486b1362c
malika <mberrehail@gmail.com>
2018-05-17 19:57:18 +0200

    exo 05

---
+++ exo_05.rb
@@ -0,0 +1,43 @@
+#{} : sert à afficher de calculer ou de mettre une variable dans un print ou puts 
+
+puts "On va compter le nombre d'heures de travail à THP" : 
+# affiche une phrase et va à la ligne 
+
+puts "Travail : #{10 * 5 * 11}" 
+# multiplie : 10 * 5 * 11 
+
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" 
+# multiplie : 10 * 5 * 11 * 60 
+
+puts "Et en secondes ?" 
+# affiche une phrase 
+
+puts 10 * 5 * 11 * 60 * 60 
+# multiplie : 10 * 5 * 11 * 60 * 60 
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" 
+# affiche une phrase 
+
+puts 3 + 2 < 5 - 7 
+# affiche résultat d'un boléen  5 est inférieur à 2 : false 
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" 
+# phrase + résulat calculer dans #{3 + 2} : 5 
+
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" 
+# phrase + résulat calculer dans #{5 - 7} : -2 
+
+puts "Ok, c'est faux alors !" 
+#affiche : Ok, c'est faux alors ! 
+
+puts "C'est drôle ça, faisons-en plus :" 
+#affiche: C'est drôle ça, faisons-en plus : 
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" 
+# calcul d'un bollean qui renvoi true 
+
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" 
+# calcul d'un bollean qui renvoi true 
+
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" 
+# calcul d'un bollean qui renvoi false 
