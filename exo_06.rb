03cfc772efa6a7d7b6beb496770f9f2df66f7c9e
malika <mberrehail@gmail.com>
2018-05-17 20:01:40 +0200

    exo 06

---
+++ exo_06.rb
@@ -0,0 +1,17 @@
+number_of_hours_worked_per_day = 10 
+number_of_days_worked_per_week = 5 
+number_of_weeks_in_THP = 11 
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" 
+
+=begin 
+creation de 3 variables on l'on attribue des valeur numerique 
+et multiplication dans un puts de ces 3 variables avec: 
+#{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP 
+=end 
+
+
+
+#puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" 
+
+#affiche un message d'erreur car la variable number_of_minutes_in_an_hour n'existe pas 
