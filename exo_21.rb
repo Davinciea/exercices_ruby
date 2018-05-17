957d653881272173aed2f656590ddd25c98dd1be
malika <mberrehail@gmail.com>
2018-05-17 20:54:06 +0200

    exo 21

---
+++ exo_21.rb
@@ -0,0 +1,13 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'etages veux-tu ?"
+nb = Integer(gets.chomp)
+
+puts "Voici la pyramide "
+
+tour = 1
+
+for num in tour..nb
+  (nb-tour).times {print " "}
+  tour.times {print"#"}
+  puts ""
+  tour +=1
+end
